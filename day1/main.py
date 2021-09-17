25016f8e06e2def4c4231ce7a48aaef0272d8a21
Dawid Kubicki <dawid@pluscode.io>
2021-09-17 12:31:56 +0200

    day1 python coding challenge

---
+++ day1/main.py
@@ -0,0 +1,7 @@
+name = input("Hello there, what's your name: ")
+print(f"Hello there {name}")
+
+city = input("What city have you been growing up? ")
+pet = input("What is your pet name? ")
+
+print(f"You live in {city} with your pet {pet}")
