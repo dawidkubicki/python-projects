fa9d29969d18feb6ce0bdb0d2c487305fcb2a22e
Dawid <dawid@pluscode.io>
2021-09-22 15:00:06 +0200

    full hangman game added

--- day3/hangman4.py
+++ day3/hangman4.py
@@ -1,4 +1,5 @@
 import random
+from words import rd_list
 
 stages = ['''
   +---+
@@ -57,49 +58,57 @@ stages = ['''
 =========
 ''']
 
-end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-word_length = len(chosen_word)
+while True:
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
+	end_of_game = False
+	word_list = ["ardvark", "baboon", "camel"]
+	chosen_word = random.choice(rd_list)
+	word_length = len(chosen_word)
 
-lives = 6
+	#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+	#Set 'lives' to equal 6.
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+	lives = 6
 
-#Create blanks
-display = []
-for _ in range(word_length):
-	display += "_"
+	#Testing code
+	#print(f'Pssst, the solution is {chosen_word}.')
 
-while not end_of_game:
-	guess = input("Guess a letter: ").lower()
 
-	for position in range(word_length):
-		letter = chosen_word[position]
-		
-		if letter==guess:
-        		display[position] = letter
+	print("Game starts now!")
 
-	if guess not in chosen_word:
-		lives-=1
-		print(f"{stages[lives]}")
 
-    #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
-	print(f"{' '.join(display)}")
 
-	if "_" not in display:
-        	end_of_game = True
-        	print("You win.")
+	#Create blanks
+	display = []
+	for _ in range(word_length):
+		display += "_"
 
-	elif lives<=0:
-		end_of_game = True
-		print("You lose.")
+	while not end_of_game:
+		guess = input("Guess a letter: ").lower()
 
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+		for position in range(word_length):
+			letter = chosen_word[position]
+			
+			if letter==guess:
+				display[position] = letter
+
+		if guess not in chosen_word:
+			lives-=1
+			print(f"{stages[lives]}")
+
+	    #TODO-2: - If guess is not a letter in the chosen_word,
+	    #Then reduce 'lives' by 1. 
+	    #If lives goes down to 0 then the game should stop and it should print "You lose."
+
+		print(f"{' '.join(display)}")
+
+		if "_" not in display:
+			end_of_game = True
+			print("You win.")
+
+		elif lives<=0:
+			end_of_game = True
+			print("You lose.")
+
+	    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
