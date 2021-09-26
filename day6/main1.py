20b185bdbbe61629f67dcf537a1874960a6eaea0
Dawid Kubicki <dawid@pluscode.io>
2021-09-26 20:54:27 +0200

    leap year checker

---
+++ day6/main1.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+    print(f_name.title(), l_name.title())
+
+f_name = str(input("What's your f_name: "))
+l_name = str(input("What's your l_name: "))
+
+format_name(f_name, l_name)
---
+++ day6/test.py
@@ -0,0 +1,13 @@
+def days_in_month(year, month_chosen):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
+
+    print(month_days[month_chosen-1])
+
+days_in_month(2021, 2)
+
+   
+
+
+
+
+
