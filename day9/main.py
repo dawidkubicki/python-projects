680208b21d44ff97ae83ccd14851c4493d879867
Dawid Kubicki <dawid@pluscode.io>
2021-10-02 13:58:41 +0200

    plan of an attack

---
+++ day9/main.py
@@ -0,0 +1,22 @@
+import art
+import game_data
+
+print(art.go)
+#print(art.vs)
+
+
+'''
+Plan of a attack: 
+
+Link to a game: https://replit.com/@appbrewery/higher-lower-final?embed=1&output=1#main.py
+
+1. Create a function reading random question
+2. Create a function comparing results
+    a) If its return True -> Add point, add a question index number to not readable agai    n questions. 
+
+    You can create a list of indexes and use it as index, and every single time you are 
+    will be reading from it use function pop to delete it. Remember it game will be starting again to update numbers of the elements.
+
+    Else return False -> Show numnber of points
+3. Create a function adding a point
+4. 
