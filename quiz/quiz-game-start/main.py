8a548264fef70b74d34acbdaa321d7298fe3290d
Dawid Kubicki <dawid@pluscode.io>
2021-10-07 18:12:46 +0200

    methods added: next_question and still_has_questions

--- quiz/quiz-game-start/main.py
+++ quiz/quiz-game-start/main.py
@@ -1,12 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question_item in question_data:
     question = Question(question_item["text"], question_item["answer"])
     question_bank.append(question)
-#
-# for question in question_bank:
-#     print(question.text)
-#     print(question.answer)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
