c4d56bd7b74d9034e21b5fa5906dddf4775693b3
Dawid Kubicki <dawid@pluscode.io>
2021-10-13 20:47:46 +0200

    random food generator

--- race_turtle/main.py
+++ race_turtle/main.py
@@ -30,13 +30,12 @@ while is_race_on:
     for turtle in all_turtle:
 
         if turtle.xcor() > 230:
+            is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_bet:
                 print(f"You won! Turtle {winning_color} was first.")
             else:
                 print(f"You lost :( \nTurtle {winning_color} was first.")
-    tim.right(10)
-            is_race_on = False
         else:
             rand_distance = random.randint(0, 10)
             turtle.forward(rand_distance)
