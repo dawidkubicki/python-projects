7eb508c39d314045fc74328d56abff17844fb8ff
Dawid Kubicki <dawid@pluscode.io>
2021-10-23 22:19:05 +0200

    high score added

--- snake_game/main.py
+++ snake_game/main.py
@@ -35,14 +35,13 @@ while game_is_on:
 
     # detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 5:
-            game_is_on = False
-            score.game_over()
-
+            score.reset()
+            snake.reset()
 
 screen.exitonclick()
--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -12,16 +12,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=270)
         self.score = 0
+        self.high_score = 0
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        msg = "Score: " + str(self.score)
+        msg = "Score: " + str(self.score) + " High Score: " + str(self.high_score)
         self.clear()
         self.write(msg, font=STYLE, align=ALIGNMENT)
 
-    def game_over(self):
-        self.goto((0,0))
-        self.write("GAME OVER", font=STYLE, align="center")
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto((0,0))
+    #     self.write("GAME OVER", font=STYLE, align="center")
 
     def increase_score(self):
         self.score += 1
