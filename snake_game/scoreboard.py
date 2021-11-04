630cf6ede762234025212b37da3b8ebbff44f78c
Dawid Kubicki <dawid@pluscode.io>
2021-11-04 18:19:54 +0000

    high scorre added

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -12,17 +12,25 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=270)
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_high_score()
         self.update_scoreboard()
 
+    def read_high_score(self):
+        with open("data.txt", mode="r") as f:
+            return f.read()
+
+    def update_high_score(self, score):
+        with open("data.txt", mode="w") as f:
+            f.write(str(score))
+
     def update_scoreboard(self):
-        msg = "Score: " + str(self.score) + " High Score: " + str(self.high_score)
+        msg = "Score: " + str(self.score) + " High Score: " + self.read_high_score()
         self.clear()
         self.write(msg, font=STYLE, align=ALIGNMENT)
 
     def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
+        if self.score > int(self.high_score):
+            self.update_high_score(self.score)
         self.score = 0
         self.update_scoreboard()
 
